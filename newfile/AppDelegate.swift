ffbbc4e17dcd7ebcc759cbba551bddb3ab2b729d
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2026-07-07 08:56:32 +0530

    Create a new untitled file in the target folder on open
    Handles application(_:open:) to receive the path OpenInTerminal passes
    via "open -a", resolves whether it's a file or folder, creates an
    untitled(.txt) file avoiding name collisions, and reveals it in Finder.

--- newfile/AppDelegate.swift
+++ newfile/AppDelegate.swift
@@ -11,17 +11,49 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+    }
+
+    func application(_ application: NSApplication, open urls: [URL]) {
+        guard let target = urls.first else {
+            NSApp.terminate(nil)
+            return
+        }
+
+        let folder = directory(for: target)
+        if let newFile = createNewFile(in: folder) {
+            NSWorkspace.shared.activateFileViewerSelecting([newFile])
+        }
+
+        NSApp.terminate(nil)
+    }
+
+    private func directory(for url: URL) -> URL {
+        var isDirectory: ObjCBool = false
+        FileManager.default.fileExists(atPath: url.path, isDirectory: &isDirectory)
+        return isDirectory.boolValue ? url : url.deletingLastPathComponent()
+    }
+
+    private func createNewFile(in folder: URL) -> URL? {
+        var name = "untitled.txt"
+        var fileURL = folder.appendingPathComponent(name)
+        var counter = 2
+
+        while FileManager.default.fileExists(atPath: fileURL.path) {
+            name = "untitled \(counter).txt"
+            fileURL = folder.appendingPathComponent(name)
+            counter += 1
+        }
+
+        let created = FileManager.default.createFile(atPath: fileURL.path, contents: nil)
+        return created ? fileURL : nil
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
 
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
-
-
 }
+
 
